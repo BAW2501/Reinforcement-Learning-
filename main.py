5bc3f9797b943d8d1cd26452d49ebb74d83529bb
Belhadj Ahmed  Walid <80895522+BAW2501@users.noreply.github.com>
2023-02-18 17:32:02 +0000

    dev-env requirements

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import gym
+from stable_baselines3 import PPO
 
 
-env = gym.make('LunarLander-v2',render_mode='rgb_array')
-
+env = gym.make('LunarLander-v2')
 env.reset()
 
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample())
+model = PPO('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=10000)
+# run for 1000 steps which is about 10 seconds
+N_EPISODES = 10
+for episode in range(N_EPISODES):
+    obs = env.reset()
+    done = False
+    while not done:
+        env.render()
+        obs , reward , done , info = env.step(env.action_space.sample())
 
 env.close()
+
+
+# download https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz with a command
+#!wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz
+# extract to /home/codespace/.mujoco/mjpro150
+# pip install mujoco-py
+# pip install gym[all]
+# pip install stable-baselines3
+# pip install pyglet==1.5.27
+# to run use 
+#  xvfb-run -s "-screen 0 1400x900x24" python main.py
+# u can install xvfb-run with sudo apt-get install xvfb
